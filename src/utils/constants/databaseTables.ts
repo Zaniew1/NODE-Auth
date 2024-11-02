cf7afbd886a7e079201989ff94000de65ca73866
Mateusz <m.zaniewski1995@gmail.com>
2024-11-02 14:44:27 +0100

    delete database class

--- src/utils/constants/databaseTables.ts
+++
@@ -1,9 +0,0 @@
-const Table = {
-  enterprise: Symbol("enterprise"),
-  department: Symbol("department"),
-  worker: Symbol("worker"),
-  training: Symbol("training"),
-  user: Symbol("user"),
-};
-
-type TableType = keyof typeof Table;
