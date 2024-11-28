cb4ed9998ee45ddd8243f426bfaa410e14dccb6d
Mateusz <m.zaniewski1995@gmail.com>
2024-11-28 20:46:31 +0100

    add assert validation on payload in authenticator middleware

--- src/middleware/authenticator.ts
+++ src/middleware/authenticator.ts
@@ -11,6 +11,7 @@ export const authenticator: RequestHandler = (req, res, next) => {
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uSWQiOiI2NzM2MzkyMDg5NjAwZDc1MWYwZDE2NDQiLCJ1c2VySWQiOiI2NzM2MzkxOTg5NjAwZDc1MWYwZDE2M2QiLCJpYXQiOjE3MzE2MDY4MTYsImV4cCI6MTczMTYwNzcxNiwiYXVkIjpbIlVzZXIiXX0.URosMZ1Zmn52wMBxaY8WZcHRgiqI6uVmI-MFrYqLGM0";
   appAssert(accessToken, HttpErrors.UNAUTHORIZED, Message.FAIL_USER_NOT_AUTHORIZED, AppErrorCode.InvalidAccessToken);
   const payload = JWT.validateAccessToken(accessToken);
+  appAssert(payload, HttpErrors.UNAUTHORIZED, Message.FAIL_USER_NOT_AUTHORIZED, AppErrorCode.InvalidAccessToken);
   res.locals.userId = payload.userId;
   res.locals.sessionId = payload.sessionId;
   next();
