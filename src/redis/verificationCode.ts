e2286566e7d46f8258a0ec3b06741bd5db67718d
Mateusz <m.zaniewski1995@gmail.com>
2025-01-17 19:44:55 +0100

    add setVerificationCodeListKey function

--- src/redis/verificationCode.ts
+++ src/redis/verificationCode.ts
@@ -1,5 +1,9 @@
 import { VerificationCodeDocument } from "../auth/model/verificationCode.model";
+import { UserDocument } from "../user/model/user.model";
 
 export const setVerificationCodeHashKey = (id: VerificationCodeDocument["_id"]) => {
   return `verificationCode#${id}`;
+};
+export const setVerificationCodeListKey = (id: UserDocument["_id"]) => {
+  return `user:verificationCodes:${id}`;
 };
