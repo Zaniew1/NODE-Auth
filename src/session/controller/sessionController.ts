6250d07b137603cd47700b48cc74a121d096e1ef
Mateusz <m.zaniewski1995@gmail.com>
2025-01-17 19:44:39 +0100

    replace mongoose methods with DatabaseClass abstract layer

--- src/session/controller/sessionController.ts
+++ src/session/controller/sessionController.ts
@@ -5,42 +5,24 @@ import catchAsync from "../../utils/helpers/catchAsync";
 import { Message } from "../../utils/constants/messages";
 import { RequestHandler, Request, Response, NextFunction } from "express";
 import { objectIdSchema } from "../../auth/zodSchemas/ObjectIdSchema";
-// import { deleteCacheSessionById } from "../../redis/session";
-import { setSessionHashKey } from "../../redis/session";
+import DatabaseClass from "../../utils/Database/Database";
 export const getSessionHandler: RequestHandler = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   const userID = objectIdSchema.parse(res.locals.userId);
   const sessionID = objectIdSchema.parse(res.locals.sessionId);
-
-  const sessions = await SessionModel.find(
-    {
-      userId: userID,
-      expiresAt: { $gt: new Date() },
-    },
-    {
-      _id: 1,
-      userAgent: 1,
-      createdAt: 1,
-    },
-    {
-      sort: { createdAt: -1 },
-    }
-  );
+  const sessions = await DatabaseClass.session.findSessionsByUserId(userID);
   appAssert(sessions.length > 0, HttpErrors.NOT_FOUND, Message.FAIL_SESSION_NOT_FOUND);
-  // set isCurrent key for current session
 
+  // set isCurrent key for current session
   const sessionsAndCurrentSession = sessions.map((session) => ({
     ...session,
     ...(session._id === sessionID && { isCurrent: true }),
   }));
+
   res.status(HttpErrors.OK).json({ sessions: sessionsAndCurrentSession });
 });
 export const deleteSessionHandler: RequestHandler = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   const sessionId = objectIdSchema.parse(req.body.id);
-  const sessionDeleted = await SessionModel.findOneAndDelete({
-    _id: sessionId,
-    userId: res.locals.userId,
-  });
-  // await deleteHashCacheById(setSessionHashKey(sessionId));
+  const sessionDeleted = await DatabaseClass.session.findByIdAndDelete(sessionId);
   appAssert(sessionDeleted, HttpErrors.NOT_FOUND, Message.FAIL_SESSION_NOT_FOUND);
   res.status(HttpErrors.OK).json({ message: Message.SUCCESS_SESSION_DELETED });
 });
--- src/user/controller/userController.ts
+++ src/user/controller/userController.ts
@@ -1,20 +1,20 @@
-import UserModel from "../model/user.model";
 import { HttpErrors } from "../../utils/constants/http";
 import appAssert from "../../utils/helpers/appAssert";
 import catchAsync from "../../utils/helpers/catchAsync";
 import { RequestHandler, Request, Response, NextFunction } from "express";
 import { Message } from "../../utils/constants/messages";
 import { objectIdSchema } from "../../auth/zodSchemas/ObjectIdSchema";
+import DatabaseClass from "../../utils/Database/Database";
 
 export const getUserHandler: RequestHandler = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   const userID = objectIdSchema.parse(res.locals.userId);
-  const user = await UserModel.findById(userID);
+  const user = await DatabaseClass.user.findById(userID);
   appAssert(user, HttpErrors.NOT_FOUND, Message.FAIL_USER_NOT_FOUND);
   res.status(HttpErrors.OK).json(user.omitPassword());
 });
 export const deleteUserHandler: RequestHandler = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   const userID = objectIdSchema.parse(res.locals.userId);
-  const user = await UserModel.findByIdAndDelete(userID);
+  const user = await DatabaseClass.user.findByIdAndDelete(userID);
   appAssert(user, HttpErrors.NOT_FOUND, Message.FAIL_USER_NOT_FOUND);
   res.status(HttpErrors.OK).json({ message: Message.SUCCESS_USER_DELETED });
 });
